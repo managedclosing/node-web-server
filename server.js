d86d8cf528316b03d29a58b1beb33a5b997ac3b6
managedclosing <managedclosing@gmail.com>
2017-03-04 16:40:20 -0600

    Add view and route for projects

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Portfolio projects here: '
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Bad request!'
